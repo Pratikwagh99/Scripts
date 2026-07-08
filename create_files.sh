1c83ba2347f2e210759da89f922667b66fa337a9
Pratik Wagh <waghpratik777@gmail.com>
2026-07-08 20:57:28 +0530

    create 1st script

---
+++ create_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create a new directory
+mkdir my_directory
+
+# Create three empty files inside the directory
+touch my_directory/file1.txt
+touch my_directory/file2.txt
+touch my_directory/file3.txt
+
+# List the contents of the directory
+ls -l my_directory
