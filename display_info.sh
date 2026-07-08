68ce49170b3da49307da996a76967d66cb403c92
Pratik Wagh <waghpratik777@gmail.com>
2026-07-08 21:10:25 +0530

    create 4th script

---
+++ display_info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Display current date
+echo "Current Date: $(date +%Y-%m-%d)"
+
+# Display current time
+echo "Current Time: $(date +%H:%M:%S)"
+
+# Display current username
+echo "Username: $(whoami)"
